1365c5506b606eb64782052bffb344259bd016da
sxxmin515 <ksb327d2@gmail.com>
2026-01-06 17:49:54 +0900

    260106

---
+++ chapter12_generic_lambda/src/com/yedam/java/ch1202/lambda/CommonExample.java
@@ -0,0 +1,20 @@
+package com.yedam.java.ch1202.lambda;
+
+public class CommonExample {
+
+	public static void main(String[] args) {
+		// 람다식의 활용 => 콜백함수
+		action((x,y)->{
+			int result = x + y;
+			System.out.println("result : " + result);
+		});
+		
+		action(Math::max);
+	}
+	
+	public static void action(Calculable cal) {
+		int x = 10;
+		int y = 4;
+		cal.calculate(x, y);
+	}
+}
--- chapter12_generic_lambda/src/com/yedam/java/ch1202/lambda/ComputerExample.java
+++ chapter12_generic_lambda/src/com/yedam/java/ch1202/lambda/ComputerExample.java
@@ -10,8 +10,8 @@ public class ComputerExample {
 		com = (x, y) -> { return x + y; };
 		// 실행하는 코드가 한줄이고 return 구문일 경우
 		// 중괄호와 함께 return 생략
-		com = (x, y) -> x + y;
-		
+		com = (x, y) -> x + y;	
 	}
+	
 
 }
